8e08db2641af5b344d36507681cdafe59202aded
Sai Shanmukkha Surapaneni <saishanmukkha@gmail.com>
2023-11-27 02:24:05 -0600

    Combined RGB and Grey Scale

--- main.py
+++ main.py
@@ -6,122 +6,102 @@ from Lib import JS_Scramble
 from Lib import row_cm
 from Lib import col_cm
 
-def encrypt_image(image, secret_key):
-    
-    # Handle Key - Generate Parameters
+def encryption_channel(channel_data, secret_key):
+    M, N = channel_data.shape
     JPDF_values = key.generate_JPDF_parameters(secret_key)
-    M = len(image)
-    N = len(image[0])
-    
-    input_image = image
-    # Dual Encrytion Loop
-    for idx in range(2):
-        # Josephus Parameter Generation, Mapping & Scrambling
-        sc_val = key.generate_scrambling_values(JPDF_values[f"g{idx}"], M, N)
-        mapping = JS_Mapping.generate_mapping(M, N, MP=sc_val["MP"], NP=sc_val["NP"], MStep=sc_val["MStep"], NStep=sc_val["NStep"])
-        scrambled_image = JS_Scramble.scramble_image(mapping, input_image)
-        
-        # Chaotic Mapping - Parameter Generation, Row Switching & Column Switching
-        cm_val = key.generate_chaotic_mapping_values(JPDF_values[f"d{idx}"])
-        rimage = row_cm.switch_rows(cm_val["mu"], cm_val["x"], np.array(scrambled_image))
-        cimage = col_cm.switch_columns(cm_val["mu"], cm_val["x"], rimage)
-        
-        input_image = cimage
-    
-    return input_image
+    input_data = channel_data
+    cm_val = { 
+        "x" : 0.19870228,
+        "mu": 3.99999999
+        }
 
-def decrypt_image(eimage:np.ndarray, secret_key):
-   
-    # Handle Key - Generate Parameters
-    JPDF_values = key.generate_JPDF_parameters(secret_key)
-    M = len(image)
-    N = len(image[0])
-    
-    # Dual Decryption Loop
-    input_image = eimage.copy()
-    for idx in range(1, -1, -1):
-        # Chaotic Mapping - Parameter Generation, Row Deswitching & Column Deswitching
-        cm_val = key.generate_chaotic_mapping_values(JPDF_values[f"d{idx}"])
-        cimage = col_cm.deswitch_columns(cm_val["mu"], cm_val["x"], input_image)
-        res = row_cm.deswitch_rows(cm_val["mu"], cm_val["x"], cimage)
-        
-        # Josephus Parameter Generation, Mapping & De-Scrambling
+    for idx in range(2):
+        # Josephus Scrambling
         sc_val = key.generate_scrambling_values(JPDF_values[f"g{idx}"], M, N)
-        mapping = JS_Mapping.generate_mapping(M, N, MP=sc_val["MP"], NP=sc_val["NP"], MStep=sc_val["MStep"], NStep=sc_val["NStep"])
-        scrambled_image = JS_Scramble.descramble_image(mapping, res)
-        input_image = np.array(scrambled_image)
-        
-    return input_image
+        mapping = JS_Mapping.generate_mapping(M, N, **sc_val)
+        scrambled_data = JS_Scramble.scramble_image(mapping, input_data)
+
+        # Chaotic Mapping
+        rimage = row_cm.switch_rows(cm_val["mu"], cm_val["x"], np.array(scrambled_data))
+        cimage = col_cm.switch_columns(cm_val["mu"], cm_val["x"], rimage)
+        input_data = cimage
+
+    return input_data
+
+def decryption_channel(channel_data, secret_key):
+    M, N = channel_data.shape
+    JPDF_values = key.generate_JPDF_parameters(secret_key)
+    input_data = np.array(channel_data)
+
+    cm_val = { 
+    "x" : 0.19870228,
+    "mu": 3.99999999
+    }
+
+    for idx in  range(1, -1, -1):
+        # Chaotic Mapping
+        cimage = col_cm.deswitch_columns(cm_val["mu"], cm_val["x"], input_data)
+        rimage = row_cm.deswitch_rows(cm_val["mu"], cm_val["x"], cimage)
+
+        # Josephus Scrambling
+        sc_val = key.generate_scrambling_values(JPDF_values[f"g{idx}"], M, N)
+        mapping = JS_Mapping.generate_mapping(M, N, **sc_val)
+        scrambled_data = JS_Scramble.descramble_image(mapping, rimage)
+        input_data = np.array(scrambled_data)
+
+    return input_data
+
+def process_color_image(image, secret_key, encryptFlag):   
+    image = np.array(image)
+    processed_channels = []
+
+    for idx in range(3):  # process each channel
+        channel_data = image[:, :, idx]
+        if encryptFlag:
+            processed_channel = encryption_channel(channel_data, secret_key)
+        else:
+            processed_channel = decryption_channel(channel_data, secret_key)
+        processed_channels.append(processed_channel)
     
+    processed_image = np.stack(processed_channels, axis=-1)
+    processed_image = Image.fromarray(processed_image.astype(np.uint8), mode="RGB")
+
+    return processed_image
+
+def process_grey_scale(image, secret_key, encryptFlag):
+    image = np.array(image)
+
+    if encryptFlag:
+        processed_image = encryption_channel(image, secret_key)
+    else:
+        processed_image = decryption_channel(image, secret_key)
     
+    processed_image = Image.fromarray(processed_image.astype(np.uint8), mode="L")
+
+    return processed_image
+
 if __name__ == "__main__":
     secret_key = "T3RTqXCNwUaIraqIbixsvzYb1W340ZXK"
-    # im = Image.open("./Images/color.jpg")
-    im = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12],[13,14,15,16]])
-    im_mode = "L"
-    # im_mode = im.mode
-    # im = im.convert("L")
-    image = np.array(im)
-    print("IMage Mode =", im_mode)
-    print("IMage Shape = ", image.shape)
-    print("\n########################\n")
-    print("Original IMage:\n", image)
-    print("\n########################\n")
-    
-    
-    if im_mode in ["L", "P"]:
-        enc_image = encrypt_image(image, secret_key)
-        enc_image = np.array(enc_image)
-        # eimage = Image.fromarray(enc_image.astype(np.uint8), mode=im_mode)
-    elif im_mode == "RGB":
-        enc_image_array = []
-        for idx in range(3):
-            unc_img = image[:, :, idx]
-            print(unc_img)
-            print("Shape = ", unc_img.shape)
-            enc_image_mat = encrypt_image(unc_img, secret_key)
-            enc_image_array.append(enc_image_mat)
-        enc_image = np.stack(enc_image_array, axis=-1)
-        eimage = Image.fromarray(enc_image.astype(np.uint8), mode="RGB")
-    else:
-        raise Exception(f"${im_mode} Unsupported Image Type. Currently supported Only Grayscale or RGB.")
-    
-    
-    # eimage.save('./Images/eimage.png')
-    
-    print("\n########################\n")
-    print("Encrypted Image:\n", enc_image)
-    print("\n########################\n")
-    
-    secret_key = "T3RTqXCNwUaIraqIbixsvzYb1W340ZXK"
-    # eim = Image.open("./Images/eimage.png")
-    eim = enc_image
-    # eim_mode = eim.mode
-    eim_mode = "L"
-    eim_mat = np.array(eim)
-    
-    
-    if eim_mode in ["L", "P"]:
-        dec_image = decrypt_image(eim_mat, secret_key)
-        dec_image = np.array(dec_image)
-        # deimage = Image.fromarray(dec_image.astype(np.uint8), mode=im_mode)
-    elif eim_mode == "RGB":
-        dec_image_array = []
-        for idx in range(3):
-            dec_imgs = eim_mat[:, :, idx]
-            print("D Image:\n", dec_imgs)
-            print("Shape = ",dec_imgs.shape)
-            dec_image_mat = decrypt_image(dec_imgs, secret_key)
-            dec_image_array.append(enc_image_mat)
-        dec_image = np.stack(dec_image_array, axis=-1)
-        print(dec_image.shape)
-        deimage = Image.fromarray(dec_image.astype(np.uint8), mode="RGB")
-    else:
-        raise Exception(f"{eim_mode} Unsupported Image Type. Currently supported Only Grayscale or RGB.")
-    
-    # deimage.save('./Images/dimage.png')
+    image = Image.open("./Images/cmk.jpeg")
 
-    print("\n########################\n")
-    print("Decrypted Image:\n", dec_image)
-    print("\n########################\n")
-    print(np.array_equal(image, dec_image))
+    if image.mode == "L":
+        # Encrypt
+        encrypted_image = process_grey_scale(image, secret_key, encryptFlag=True)
+        encrypted_image.save('./Images/eimage.png')
+
+        # Decrypt
+        decrypted_image = process_grey_scale(encrypted_image, secret_key, encryptFlag=False)
+        decrypted_image.save('./Images/dimage.png')
+        
+    elif image.mode == "RGB":
+        # Encrypt
+        encrypted_image = process_color_image(image, secret_key, encryptFlag=True)
+        encrypted_image.save('./Images/eimage.png')     
+
+        # Decrypt
+        decrypted_image = process_color_image(encrypted_image, secret_key, encryptFlag=False)
+        decrypted_image.save('./Images/dimage.png')
+
+    
+
+    
--- testrgb.py
+++ testrgb.py
@@ -6,63 +6,40 @@ from Lib import JS_Scramble
 from Lib import row_cm
 from Lib import col_cm
 
-
-def gen_rgb():
-    a = [[ 1,  2,  3,  4],
-        [ 5,  6,  7,  8],
-        [ 9, 10, 11, 12],
-        [13, 14, 15, 16]]
-    
-    a = np.array(a)
-
-    lst = [a,a,a]
-
-    rb = np.stack(lst, axis=-1)
-
-    print(rb)
-    return rb
-
-# Assuming other functions and modules (key, JS_Mapping, JS_Scramble, row_cm, col_cm) are already defined as per your provided code
-
-def encrypt_decrypt_channel(channel_data, secret_key, encrypt=True):
+def encryption_channel(channel_data, secret_key):
     M, N = channel_data.shape
     JPDF_values = key.generate_JPDF_parameters(secret_key)
-    # print(JPDF_values)
     input_data = channel_data
-    
-    range_func = range(2)  # for encryption
+    cm_val = { 
+        "x" : 0.19870228,
+        "mu": 3.99999999
+        }
 
-    for idx in range_func:
+    for idx in range(2):
         # Josephus Scrambling
         sc_val = key.generate_scrambling_values(JPDF_values[f"g{idx}"], M, N)
         mapping = JS_Mapping.generate_mapping(M, N, **sc_val)
         scrambled_data = JS_Scramble.scramble_image(mapping, input_data)
 
         # Chaotic Mapping
-        cm_val = { 
-        "x" : 0.19870228,
-        "mu": 3.99999999
-        }
-
         rimage = row_cm.switch_rows(cm_val["mu"], cm_val["x"], np.array(scrambled_data))
         cimage = col_cm.switch_columns(cm_val["mu"], cm_val["x"], rimage)
         input_data = cimage
 
     return input_data
 
-def edec_channel(channel_data, secret_key, encrypt=True):
+def decryption_channel(channel_data, secret_key):
     M, N = channel_data.shape
     JPDF_values = key.generate_JPDF_parameters(secret_key)
-    # print(JPDF_values)
     input_data = np.array(channel_data)
-    print(input_data.shape)
-    range_func = range(1, -1, -1)  # for decryption
-    # Chaotic Mapping
+
     cm_val = { 
     "x" : 0.19870228,
     "mu": 3.99999999
     }
-    for idx in range_func:
+
+    for idx in  range(1, -1, -1):
+        # Chaotic Mapping
         cimage = col_cm.deswitch_columns(cm_val["mu"], cm_val["x"], input_data)
         rimage = row_cm.deswitch_rows(cm_val["mu"], cm_val["x"], cimage)
 
@@ -74,42 +51,33 @@ def edec_channel(channel_data, secret_key, encrypt=True):
 
     return input_data
 
-def process_image(image, secret_key, encrypt=True):
-    # im = Image.open(image_path)
-    
-    # print(image.shape)
-    # if im.mode != "RGB":
-    #     raise ValueError("Only RGB images are supported.")
-    
+def process_image(image, secret_key, encrypt=True):   
     image = np.array(image)
     processed_channels = []
 
     for idx in range(3):  # process each channel
         channel_data = image[:, :, idx]
         if encrypt:
-            processed_channel = encrypt_decrypt_channel(channel_data, secret_key, encrypt)
+            processed_channel = encryption_channel(channel_data, secret_key)
         else:
-            processed_channel = edec_channel(channel_data, secret_key, encrypt)
+            processed_channel = decryption_channel(channel_data, secret_key)
         processed_channels.append(processed_channel)
-    if(encrypt):
-        print("Encryption\n:")
-    else:
-        print("Decryption\n")
-    for ch in processed_channels:
-        print(ch)
+    
     processed_image = np.stack(processed_channels, axis=-1)
     processed_image = Image.fromarray(processed_image.astype(np.uint8), mode="RGB")
 
     return processed_image
 
-# Example Usage
-secret_key = "T3RTqXCNwUaIraqIbixsvzYb1W340ZXK"
-image = Image.open("./Images/cmk.jpeg")
-# Encrypt
-encrypted_image = process_image(image, secret_key, encrypt=True)
-encrypted_image.save('./Images/eimage.png')
-print(encrypted_image)
-# Decrypt
-decrypted_image = process_image(encrypted_image, secret_key, encrypt=False)
-decrypted_image.save('./Images/dimage.png')
-print(decrypted_image)
+if __name__ == "__main__":
+    secret_key = "T3RTqXCNwUaIraqIbixsvzYb1W340ZXK"
+    image = Image.open("./Images/test.jpg")
+
+    # Encrypt
+    encrypted_image = process_image(image, secret_key, encrypt=True)
+    encrypted_image.save('./Images/eimage.png')
+    print(encrypted_image)
+
+    # Decrypt
+    decrypted_image = process_image(encrypted_image, secret_key, encrypt=False)
+    decrypted_image.save('./Images/dimage.png')
+    print(decrypted_image)
